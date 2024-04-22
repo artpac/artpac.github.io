d6201e4c4559f0a98bd0e7b7b285ad420399ffd4
Artur Pacut <arturpacut904@gmail.com>
2024-04-22 21:34:24 +0100

    Landing Page
    Finished landing page, main page
    Uploaded Milan photos.
    Created javascript for landing page and main page

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-function displayBox() {
+function displayUkBox() {
     document.getElementById("ukBox").style.display = "block";
 }
-
-function hideBox() {
+function hideUkBox() {
     document.getElementById("ukBox").style.display = "none";
+}
+function displayGermanyBox() {
+    document.getElementById("germanyBox").style.display = "block";
+}
+function hideGermanyBox() {
+    document.getElementById("germanyBox").style.display = "none";
+}
+function displayPolandBox() {
+    document.getElementById("polandBox").style.display = "block";
+}
+function hidePolandBox() {
+    document.getElementById("polandBox").style.display = "none";
+}
+function displayItalyBox() {
+    document.getElementById("italyBox").style.display = "block";
+}
+function hideItalyBox() {
+    document.getElementById("italyBox").style.display = "none";
+}
+function displayDenmarkBox() {
+    document.getElementById("denmarkBox").style.display = "block";
+}
+function hideDenmarkBox() {
+    document.getElementById("denmarkBox").style.display = "none";
+}
+
+
+function zoomIn(element){
+    element.style.zIndex = '-1';
+    element.style.transform = 'scale(1.2)';
+}
+function zoomOut(element){
+    element.style.transform = 'scale(1)';
+    element.style.zIndex = 'auto';
 }
