b30272253f58ad73dde7d07f35c3f66f512fb2cc
Artur Pacut <arturpacut904@gmail.com>
2024-04-24 17:27:02 +0100

    Finished Website

--- script.js
+++ script.js
@@ -31,10 +31,67 @@ function hideDenmarkBox() {
 
 
 function zoomIn(element){
-    element.style.zIndex = '-1';
     element.style.transform = 'scale(1.2)';
 }
 function zoomOut(element){
     element.style.transform = 'scale(1)';
-    element.style.zIndex = 'auto';
+}
+
+
+function viewGallery() {
+    document.getElementById('countryGallery').style.display = 'block';
+}
+// Function to close popup
+function hideGallery() {
+    document.getElementById('countryGallery').style.display = 'none';
+}
+
+function viewBabiaGoraGallery() {
+    document.getElementById('babiaGoraGallery').style.display = 'block';
+}
+// Function to close popup
+function hideBabiaGoraGallery() {
+    document.getElementById('babiaGoraGallery').style.display = 'none';
+}
+function viewKrakowGallery() {
+    document.getElementById('krakowGallery').style.display = 'block';
+}
+// Function to close popup
+function hideKrakowGallery() {
+    document.getElementById('krakowGallery').style.display = 'none';
+}
+function viewLondonGallery() {
+    document.getElementById('londonGallery').style.display = 'block';
+}
+// Function to close popup
+function hideLondonGallery() {
+    document.getElementById('londonGallery').style.display = 'none';
+}
+function viewObanGallery() {
+    document.getElementById('obanGallery').style.display = 'block';
+}
+// Function to close popup
+function hideObanGallery() {
+    document.getElementById('obanGallery').style.display = 'none';
+}
+function viewInvernessGallery() {
+    document.getElementById('invernessGallery').style.display = 'block';
+}
+// Function to close popup
+function hideInvernessGallery() {
+    document.getElementById('invernessGallery').style.display = 'none';
+}
+function viewSkyeGallery() {
+    document.getElementById('skyeGallery').style.display = 'block';
+}
+// Function to close popup
+function hideSkyeGallery() {
+    document.getElementById('skyeGallery').style.display = 'none';
+}
+function viewPitlochryGallery() {
+    document.getElementById('pitlochryGallery').style.display = 'block';
+}
+// Function to close popup
+function hidePitlochryGallery() {
+    document.getElementById('pitlochryGallery').style.display = 'none';
 }
